ebc7f369e30b83cb2ca1805fe0ed3abd8272c891
slewis1967 <sean@lewisproperties.com.au>
2025-09-08 17:36:30 +1000

    Update LogoutButton.tsx

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,1 +1,18 @@
+// src/components/LogoutButton.tsx
+'use client'
 
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
+import { Button } from './ui/button'
+
+export default function LogoutButton() {
+  const router = useRouter()
+  const supabase = createClientComponentClient()
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut()
+    router.refresh()
+  }
+
+  return <Button onClick={handleSignOut} variant="outline">Logout</Button>
+}
